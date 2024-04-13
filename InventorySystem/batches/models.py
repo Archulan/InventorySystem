f9172c65d6f241102225433bf8cf999c79c391ca
archulan <archulan@feasto.io>
2024-04-13 14:56:56 +1000

    Define and register models

--- InventorySystem/batches/models.py
+++ InventorySystem/batches/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from items.models import Item
 
-# Create your models here.
+class Batch(models.Model):
+    batch_code = models.CharField(max_length=100)
+    date_received = models.DateField()
+    quantity = models.IntegerField()
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='batches')
+
+    def __str__(self):
+        return self.batch_code
