af5f2866acbb23ff9116e96b6c3538d5c7f8f33b
archulan <archulan@feasto.io>
2024-04-13 19:27:59 +1000

    Set Up URL Routing

--- InventorySystem/batches/views.py
+++ InventorySystem/batches/views.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from .models import Batch
 from .serializers import BatchSerializer
 
-class ItemViewSet(viewsets.ModelViewSet):
+class BatchViewSet(viewsets.ModelViewSet):
     queryset = Batch.objects.all()
     serializer_class = BatchSerializer
